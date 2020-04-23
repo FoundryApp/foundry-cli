a4f1d9ffe74d1ee8d8d6e905dc89a61af581f378
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2020-04-23 17:56:12 +0200

    Update install script

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+
+# env GOOS=darwin GOARCH=amd64 go build -o ./build/foundry-macos-x86_64
+# env GOOS=linux GOARCH=amd64 go build -o ./build/foundry-linux-x86_64
+# env GOOS=linux GOARCH=arm64 go build -o ./build/foundry-linux-arm64
+
 flags=$@
 build_name="foundry"
 
